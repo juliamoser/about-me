4bb975a81e69f692f3c35283bbdc6be6d7f182c0
juliamoser <juliamo@comcast.net>
2018-07-10 15:15:00 -0700

    Create myjs.js

---
+++ myjs.js
@@ -0,0 +1,26 @@
+
+
+
+document.addEventListener("DOMContentLoaded",
+function(){
+    //all da code goes here teehee
+//alert("Hello World!");
+//console.log("message");
+
+let myPhoto =
+document.getElementById("cool")
+document.getElementById("fb")
+document.getElementById("twotter")
+
+//myPhoto.addEventListener("click", changeLink)
+
+
+
+//function changeLink(){
+    //<a href="https://www.w3schools.com">
+//<alt="W3Schools" src="download.png">
+//    console.log('hey');
+    //myPhoto.src = "flowers.jpg"
+}
+
+}) //end of DOMContentLoaded
